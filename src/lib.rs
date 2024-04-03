dab10f3e4317ff03078edac7541c8d2fab10c4a9
Robert Jördens <rj@quartiq.de>
2024-04-03 14:35:53 +0200

    add std HashSet, HashMap

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,21 @@
 //! Fowler-Noll-Vo Hashes
-#![no_std]
+//!
+//! The implementation here is fully `no_std` and `no_alloc` and implements both FNV-1 and FNV-1a
+//! for `u32`, `u64`, and `u128` hash sizes.
+//!
+//! See also the following crates:
+//! * [`fnv`](https://doc.servo.org/fnv/)
+//! * [`fnv-rs`](https://docs.rs/fnv_rs/latest/fnv_rs/)
+//! * [`const-fnv1a-hash`](https://docs.rs/const-fnv1a-hash/latest/const_fnv1a_hash/)
+#![cfg_attr(not(feature = "std"), no_std)]
 #![warn(missing_docs, rust_2018_idioms)]
 #![forbid(unsafe_code)]
 
+use core::hash::{BuildHasherDefault, Hasher};
 use core::ops::BitXor;
 use num_traits::{AsPrimitive, WrappingMul};
+#[cfg(feature = "std")]
+use std::collections::{HashMap, HashSet};
 
 /// Fowler-Noll-Vo Hashes
 ///
@@ -13,8 +24,9 @@ use num_traits::{AsPrimitive, WrappingMul};
 /// Note that:
 /// * FNV is not a cryptographic hash.
 /// * FNV is not resistant to "hash flooding" denial-of-service attacks.
-pub trait Fnv: 'static + Copy + WrappingMul + BitXor<Output = Self>
+pub trait Fnv
 where
+    Self: 'static + Copy + WrappingMul + BitXor<Output = Self>,
     u8: AsPrimitive<Self>,
 {
     /// The FNV prime
@@ -23,8 +35,9 @@ where
     const OFFSET_BASIS: Self;
 
     /// Compute the Fowler-Noll-Vo hash FNV-1 (multiply before xor)
-    fn fnv1(data: impl Iterator<Item = u8>) -> Self {
-        data.fold(<Self as Fnv>::OFFSET_BASIS, |hash, byte| {
+    #[inline]
+    fn fnv1(self, data: impl Iterator<Item = u8>) -> Self {
+        data.fold(self, |hash, byte| {
             hash.wrapping_mul(&<Self as Fnv>::PRIME) ^ byte.as_()
         })
     }
@@ -34,19 +47,21 @@ where
     /// ```
     /// use fnv::Fnv;
     ///
-    /// /// Test vectors from
-    /// /// https://datatracker.ietf.org/doc/draft-eastlake-fnv/21/
+    /// // Test vectors from
+    /// // https://datatracker.ietf.org/doc/draft-eastlake-fnv/21/
     /// for (data, h32, h64) in [
     ///     ("", 0x811c9dc5, 0xcbf29ce484222325),
     ///     ("a", 0xe40c292c, 0xaf63dc4c8601ec8c),
     ///     ("foobar", 0xbf9cf968, 0x85944171f73967e8),
     /// ] {
-    ///     assert_eq!(u32::fnv1a(data.as_bytes().iter().copied()), h32);
-    ///     assert_eq!(u64::fnv1a(data.as_bytes().iter().copied()), h64);
+    ///     let data = data.as_bytes().iter().copied();
+    ///     assert_eq!(u32::OFFSET_BASIS.fnv1a(data.clone()), h32);
+    ///     assert_eq!(u64::OFFSET_BASIS.fnv1a(data.clone()), h64);
     /// }
     /// ```
-    fn fnv1a(data: impl Iterator<Item = u8>) -> Self {
-        data.fold(<Self as Fnv>::OFFSET_BASIS, |hash, byte| {
+    #[inline]
+    fn fnv1a(self, data: impl Iterator<Item = u8>) -> Self {
+        data.fold(self, |hash, byte| {
             (hash ^ byte.as_()).wrapping_mul(&<Self as Fnv>::PRIME)
         })
     }
@@ -64,3 +79,55 @@ impl Fnv for u128 {
     const PRIME: u128 = 0x0000000001000000000000000000013B;
     const OFFSET_BASIS: u128 = 0x6c62272e07bb014262b821756295c58d;
 }
+
+/// Fowler-Noll-Vo FNV-1a hash function
+///
+/// ```
+/// use core::hash::Hasher;
+/// use fnv::Fnv1aHasher;
+///
+/// // Test vector from https://datatracker.ietf.org/doc/draft-eastlake-fnv/21/
+/// let mut h = Fnv1aHasher::default();
+/// h.write("foobar".as_bytes());
+/// assert_eq!(h.finish(), 0x85944171f73967e8);
+/// ```
+pub struct Fnv1aHasher(u64);
+
+impl Fnv1aHasher {
+    /// Create an FNV-1a hasher starting with a state corresponding
+    /// to the hash `key`.
+    #[inline]
+    pub fn with_key(key: u64) -> Fnv1aHasher {
+        Fnv1aHasher(key)
+    }
+}
+
+impl Default for Fnv1aHasher {
+    #[inline]
+    fn default() -> Fnv1aHasher {
+        Self::with_key(u64::OFFSET_BASIS)
+    }
+}
+
+impl Hasher for Fnv1aHasher {
+    #[inline]
+    fn finish(&self) -> u64 {
+        self.0
+    }
+
+    #[inline]
+    fn write(&mut self, bytes: &[u8]) {
+        self.0 = self.0.fnv1a(bytes.iter().copied());
+    }
+}
+
+/// A builder for default FNV-1a hasher.
+pub type Fnv1aBuildHasher = BuildHasherDefault<Fnv1aHasher>;
+
+/// A `HashMap` using a default FNV-1a hasher.
+#[cfg(feature = "std")]
+pub type Fnv1aHashMap<K, V> = HashMap<K, V, Fnv1aBuildHasher>;
+
+/// A `HashSet` using a default FNV-1a hasher.
+#[cfg(feature = "std")]
+pub type Fnv1aHashSet<T> = HashSet<T, Fnv1aBuildHasher>;
