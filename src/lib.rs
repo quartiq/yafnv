583c9754d791a18e1fbe0661e743658a186ff65d
Robert Jördens <rj@quartiq.de>
2024-04-03 14:00:17 +0200

    make it a trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,66 @@
 //! Fowler-Noll-Vo Hashes
-//!
-//! Both FNV-1 and FNV-1a are implemented for `u32`, `u64`, and `u128`.
-//!
-//! FNV is not a cryptographic hash.
-//!
-//! FNV is not resistant to "hash flooding" denial-of-service attacks.
 #![no_std]
 #![warn(missing_docs, rust_2018_idioms)]
+#![forbid(unsafe_code)]
 
-use paste::paste;
+use core::ops::BitXor;
+use num_traits::{AsPrimitive, WrappingMul};
 
-macro_rules! fnv_impl {
-    ($ty:ty, $size:literal, $prime:literal, $offset:literal) => {
-        paste! {
-            /// Compute the Fowler-Noll-Vo hash FNV-1 of a slice.
-            pub fn [<fnv1_ $size>](data: &[u8]) -> $ty {
-                data.iter().fold($offset, |hash, byte| hash.wrapping_mul($prime) ^ *byte as $ty)
-            }
+/// Fowler-Noll-Vo Hashes
+///
+/// Both FNV-1 and FNV-1a are provided.
+///
+/// FNV is not a cryptographic hash.
+///
+/// FNV is not resistant to "hash flooding" denial-of-service attacks.
+pub trait Fnv: 'static + Copy + WrappingMul + BitXor<Output = Self>
+where
+    u8: AsPrimitive<Self>,
+{
+    /// The FNV prime
+    const PRIME: Self;
+    /// The FNV offset basis
+    const OFFSET: Self;
 
-            /// Compute the Fowler-Noll-Vo hash FNV-1a of a slice.
-            pub fn [<fnv1a_ $size>](data: &[u8]) -> $ty {
-                data.iter().fold($offset, |hash, byte| (hash ^ *byte as $ty).wrapping_mul($prime))
-            }
-        }
-    };
+    /// Compute the Fowler-Noll-Vo hash FNV-1
+    fn fnv1(data: impl Iterator<Item = u8>) -> Self {
+        data.fold(<Self as Fnv>::OFFSET, |hash, byte| {
+            hash.wrapping_mul(&<Self as Fnv>::PRIME) ^ byte.as_()
+        })
+    }
+
+    /// Compute the Fowler-Noll-Vo hash FNV-1a
+    ///
+    /// ```
+    /// use fnv::Fnv;
+    ///
+    /// /// Test vectors from
+    /// /// https://datatracker.ietf.org/doc/draft-eastlake-fnv/21/
+    /// for (data, h32, h64) in [
+    ///     ("", 0x811c9dc5, 0xcbf29ce484222325),
+    ///     ("a", 0xe40c292c, 0xaf63dc4c8601ec8c),
+    ///     ("foobar", 0xbf9cf968, 0x85944171f73967e8),
+    /// ] {
+    ///     assert_eq!(u32::fnv1a(data.as_bytes().iter().copied()), h32);
+    ///     assert_eq!(u64::fnv1a(data.as_bytes().iter().copied()), h64);
+    /// }
+    /// ```
+    fn fnv1a(data: impl Iterator<Item = u8>) -> Self {
+        data.fold(<Self as Fnv>::OFFSET, |hash, byte| {
+            (hash ^ byte.as_()).wrapping_mul(&<Self as Fnv>::PRIME)
+        })
+    }
 }
 
-fnv_impl! {u32, 32, 0x01000193, 0x811c9dc5}
-fnv_impl! {u64, 64, 0x00000100000001B3, 0xcbf29ce484222325}
-fnv_impl! {u128, 128, 0x0000000001000000000000000000013B, 0x6c62272e07bb014262b821756295c58d}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    /// Test vectors from
-    /// https://datatracker.ietf.org/doc/draft-eastlake-fnv/21/
-    #[test]
-    fn test() {
-        assert_eq!(fnv1a_32("".as_bytes()), 0x811c9dc5);
-        assert_eq!(fnv1a_32("a".as_bytes()), 0xe40c292c);
-        assert_eq!(fnv1a_32("foobar".as_bytes()), 0xbf9cf968);
-
-        assert_eq!(fnv1a_64("".as_bytes()), 0xcbf29ce484222325);
-        assert_eq!(fnv1a_64("a".as_bytes()), 0xaf63dc4c8601ec8c);
-        assert_eq!(fnv1a_64("foobar".as_bytes()), 0x85944171f73967e8);
-    }
+impl Fnv for u32 {
+    const PRIME: u32 = 0x01000193;
+    const OFFSET: u32 = 0x811c9dc5;
+}
+impl Fnv for u64 {
+    const PRIME: u64 = 0x00000100000001B3;
+    const OFFSET: u64 = 0xcbf29ce484222325;
+}
+impl Fnv for u128 {
+    const PRIME: u128 = 0x0000000001000000000000000000013B;
+    const OFFSET: u128 = 0x6c62272e07bb014262b821756295c58d;
 }
